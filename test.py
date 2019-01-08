955f62d66519a5904728a75cebb6ed8d23f5fa51
orangepiratee <orangepiratee@protonmail.com>
2019-01-08 01:08:29 -0800

    init commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import os
+
+def portScan(port='554',ip='1.1.110.0/24'):
+    if not os.path.exists(os.getcwd()+'/data'):
+        os.popen('mkdir data')
+    fname = 'data/{}_{}.txt'.format(port, ip.replace('.', '_').replace('/', '_'))
+    command = 'zmap -B 10M -p {} {} -o {}'.format(port,ip,fname)
+    text = os.popen(command)
+    print('finished')
+    #save2txt(text,fname)
+
+if __name__ == '__main__':
+    portScan()
